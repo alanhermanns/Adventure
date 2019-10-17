eeef91453c32a9523c740de51144f42c225631ae
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-17 16:25:29 -0700

    results interactiv and what

--- space-adventures/spaces.js
+++ space-adventures/spaces.js
@@ -33,18 +33,13 @@ for (let i = 0; i < manifold.choices.length; i++) {
 }
 document.body.appendChild(choicesList);
 
-choicesList.addEventListener('click', () => {
-    // choicesList.classList.add('hidden');
-    // const results = document.createElement('p');
-    // let choiceId = choicesList.value;
-    // let chosenPathResult;
-    // for (let i = 0; i < manifold.choices.length; i++) {
-    //     if (choiceId = manifold.choices[i].id){
-    //         chosenPathResult = manifold.choices[i].result;
-    //     }
-    // }
-     
+const radiOs = choicesList.querySelectorAll('input');
 
+for (let i = 0; i < radiOs.length; i++){
+    radiOs[i].addEventListener('click', () => {
+        let results = document.createElement('p');
+        results.textContent = manifold.choices[i].result; 
+        document.body.appendChild(results);
+
+    });
 }
-
-
