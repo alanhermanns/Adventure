a7e5a12f7e6aee589b9ac4230ba948f2a0ab5564
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-17 17:35:07 -0700

    tweak

--- final-dest/final.js
+++ final-dest/final.js
@@ -3,7 +3,7 @@ setTimeout(() => {
     meta.textContent = 'Meta';
     document.body.appendChild(meta);
     const button = document.createElement('button');
-    button.textContent = 'Overly Meta : Clear Local Storage. Did This Detract from Your User Experience?';
+    button.textContent = 'Overly Meta : Clear Local Storage. This will definitely detract from your user expierence.';
     button.addEventListener('click', () => {
         localStorage.clear();
         window.location = '../home/index.html';
